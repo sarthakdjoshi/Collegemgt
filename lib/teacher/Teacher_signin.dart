d8183e7e591d1caeace066698d1741c5eb07edda
sarthakjoshi <sarthakdjoshi20@gmail.com>
2024-01-20 18:41:49 +0530

    FIRST

--- lib/teacher/Teacher_signin.dart
+++ lib/teacher/Teacher_signin.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -10,6 +12,7 @@ class _Teacher_SigininState extends State<Teacher_Siginin> {
   var email = TextEditingController();
   var pass = TextEditingController();
   var abc = true;
+  CollectionReference users = FirebaseFirestore.instance.collection('Teachers');
 
   @override
   Widget build(BuildContext context) {
@@ -19,45 +22,92 @@ class _Teacher_SigininState extends State<Teacher_Siginin> {
         centerTitle: true,
         backgroundColor: Colors.amber,
       ),
-      body: Column(
-        children: [
-          const Text(
-            "Teacher Login Here",
-            style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.w900,
-                color: Colors.cyanAccent),
-          ),
-          SizedBox(
-              height: 200, child: Image.asset("assets/images/teacher.png")),
-          TextField(
-            controller: email,
-            maxLength: 10,
-            decoration: const InputDecoration(labelText: "Enter Your Email.",
-            prefixIcon: Icon(Icons.email),
-              prefixIconColor: Colors.indigoAccent,
-          ),
-          ),
-          SizedBox(height: 20),
-          TextField(
-            controller: pass,
-            obscureText: abc,
-            obscuringCharacter: "*",
-            decoration: InputDecoration(
-              label: const Text("Enter Password"),
-              prefixIcon: IconButton(
-                onPressed: () {
-                  abc = !abc;
-                  setState(() {});
+      body: FutureBuilder<QuerySnapshot>(
+        future: users.get(),
+        builder: (context, snapshot) {
+          if(snapshot.connectionState==ConnectionState.waiting){
+            return const CircularProgressIndicator();
+          }else if(snapshot.hasError){
+            return  Text("Error:${snapshot.error}");
+          }else{
+            List<DocumentSnapshot> documents=snapshot.data!.docs;
+            return ListView.builder(
+              itemCount: documents.length,
+                itemBuilder:(context, index) {
+                  Map<String,dynamic>data=documents[index].data() as Map<String,dynamic>;
+                  return Column(
+                    children: [
+                      const Text(
+                        "Teacher Login Here",
+                        style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.w900,
+                            color: Colors.cyanAccent),
+                      ),
+                      SizedBox(
+                          height: 200, child: Image.asset("assets/images/teacher.png")),
+                      TextField(
+                        controller: email,
+                        decoration: const InputDecoration(
+                          labelText: "Enter Your Email.",
+                          prefixIcon: Icon(Icons.email),
+                          prefixIconColor: Colors.indigoAccent,
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      TextField(
+                        controller: pass,
+                        obscureText: abc,
+                        obscuringCharacter: "*",
+                        decoration: InputDecoration(
+                          label: const Text("Enter Password"),
+                          prefixIcon: IconButton(
+                            onPressed: () {
+                              abc = !abc;
+                              setState(() {});
+                            },
+                            icon: const Icon(Icons.password),
+                          ),
+                          prefixIconColor: Colors.cyan,
+                        ),
+                      ),
+                      ElevatedButton(onPressed: () {
+                        if(email.text.toString()==data['email']){
+                          print("true");
+                        }else{print("False");}
+                      }, child: const Text("Login")),
+                      CupertinoButton(
+                          onPressed: () {
+                            try {
+                              if (email.text == "") {
+                                ScaffoldMessenger.of(context)
+                                    .showSnackBar(const SnackBar(
+                                  content: Text("Enter Email "),
+                                  duration: Duration(seconds: 2),
+                                ));
+                              } else {
+                                FirebaseAuth.instance
+                                    .sendPasswordResetEmail(email: email.text.toString());
+                                ScaffoldMessenger.of(context)
+                                    .showSnackBar(const SnackBar(
+                                  content: Text("Reset Link Has Sent To Your Email"),
+                                  duration: Duration(seconds: 2),
+                                ));
+                              }
+                            } on FirebaseAuthException catch (e) {
+                              print(e.code.toString());
+                            }
+                          },
+                          child: const Text("Forgot Password")),
+
+                    ],
+                  );
                 },
-                icon: const Icon(Icons.password),
-              ),
-              prefixIconColor: Colors.cyan,
-            ),
-          ),
-          ElevatedButton(onPressed: () {}, child: const Text("Login"))
-        ],
-      ),
+
+            );
+          }
+        },
+      )
     );
   }
 }
